7c78c3960ddc9cb576ce98a34e33833c1ae37f0c
Qu1gs <mikequigers@gmail.com>
2020-12-17 02:14:16 +0000

    initial commit

---
+++ src/components/Main.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+    <v-text-field
+      v-model="inputtedUser"
+      label="Search User..."
+      solo
+    ></v-text-field>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios"
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        authKey: "",
+        inputtedUser: "qu1gs",
+        userData: null
+      }
+    },
+   methods: {
+     retrieveData () {
+     let baseURL = "https://api.github.com"
+     let username = this.inputtedUser
+     var url = baseURL + "/users" + username
+     axios.get(url,{
+       headers: {
+         authorization: "token "+ this.token
+       },
+       timeout: 1000
+     })
+     .then(response => {
+       this.userData = response
+     })
+     }
+  }
+}
+</script>
