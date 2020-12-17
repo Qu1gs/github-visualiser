3fb85fbf177ac40a79a856cb76c53be8c2124e13
Qu1gs <mikequigers@gmail.com>
2020-12-17 18:13:29 +0000

    Added search and displaying of data from a searched user

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,23 @@
 <template>
-  <v-container>
+  <v-container class="container" fluid>
+    <v-row>
+    <v-app-bar class="appbar">Github Visualiser | Michael Quigley</v-app-bar>
+    </v-row>
+    <v-row>
+    <v-col class ="column">
+    <v-row>
     <v-text-field
       v-model="inputtedUser"
       label="Search User..."
       solo
     ></v-text-field>
+    <v-btn @click.native="retrieveData()"><v-icon>mdi-search</v-icon>Search</v-btn>
+    </v-row>
+      </v-col>
+      <v-col>
+        {{ this.userData }}
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -14,7 +27,6 @@ import axios from "axios"
     name: 'HelloWorld',
     data () {
       return {
-        authKey: "",
         inputtedUser: "qu1gs",
         userData: null
       }
@@ -23,17 +35,31 @@ import axios from "axios"
      retrieveData () {
      let baseURL = "https://api.github.com"
      let username = this.inputtedUser
-     var url = baseURL + "/users" + username
+     var url = baseURL + "/users/" + username
      axios.get(url,{
        headers: {
-         authorization: "token "+ this.token
+         authorization: "token "+ process.env.VUE_APP_API_KEY
        },
        timeout: 1000
      })
      .then(response => {
        this.userData = response
+       console.log(this.userData)
      })
      }
   }
 }
 </script>
+<style scoped>
+.container{
+  margin:0;
+  padding:0;
+}
+.appbar{
+  content: center;
+}
+.column{
+  margin-left:2%;
+  margin-top:1%;
+}
+</style>
