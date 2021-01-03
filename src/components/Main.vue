55de18ddfdfc6fb0650829840f6ad82ec7da343f
Qu1gs <mikequigers@gmail.com>
2021-01-03 04:52:46 +0000

    Fetched data for graphs

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,9 +5,10 @@
     </v-row>
     <v-row>
     <v-col class="column">
-    <v-card class="searchCard mx-auto" align="center" justify="center">
+    <v-card class="searchCard mx-auto" align="center" justify="center" max-width="900px">
     <v-row>
-    <v-text-field height="50px"
+    <v-text-field 
+      height="50px"
       v-model="inputtedUser"
       label="Search User..."
       solo
@@ -16,26 +17,28 @@
     <v-card v-if="repoData">{{ this.commitData }}</v-card>
     </v-row>
     </v-card>
+    {{ this.repoCommits }}
+    {{ this.languages }}
       </v-col>
-      <v-col>
-      <v-simple-table v-if="login">
+      <v-col v-if="login">
+        <v-card max-width="900px">
+        <v-row>
+          <v-col class="justify-center">
+        <v-img :src=avatar max-width="200px"></v-img>
+          </v-col>
+          <v-col>
+        <v-card-title>User Information</v-card-title>
+          <v-card-subtitle>
+            <br>Login: {{ login }}
+            <br>Full Name: {{ fullName }}
+            <br>Company: {{ company }}
+         </v-card-subtitle>
+         </v-col>
+        </v-row>
+        </v-card>
+        <v-card max-width="900px">
+      <v-simple-table>
         <tbody>
-          <tr>
-            <td>Login:</td>
-            <td>{{ this.login }}</td>
-          </tr>
-          <tr>
-            <td>Avatar:</td>
-            <td>{{ this.avatar }}</td>
-          </tr>
-          <tr>
-            <td>Full Name:</td>
-            <td>{{ this.fullName }}</td>
-          </tr>
-          <tr>
-            <td>Company:</td>
-            <td>{{ this.company }}</td>
-          </tr>
           <tr>
             <td>Looking for Work:</td>
             <td>{{ this.lookingForWork }}</td>
@@ -70,6 +73,7 @@
           </tr>
         </tbody>
       </v-simple-table>
+      </v-card>
       </v-col>
     </v-row>
   </v-container>
@@ -81,6 +85,7 @@ import axios from "axios"
     name: 'Main',
     data () {
       return {
+        baseURL: "https://api.github.com",
         inputtedUser: "qu1gs",
         userData: null,
         displayData: false,
@@ -97,24 +102,25 @@ import axios from "axios"
         noOfFollowing: null,
         dateCreated: null,
         repoData: null,
-        commitData: []
+        listOfRepos: null,
+        repoCommits: [],
+        languages: []
       }
     },
    methods: {
      submit(){
+      this.commitNames = null
+      this.commitValues = []
       this.retrieveData()
-      this.retrieveRepoData()
-      this.retrieveAllData()
+      this.retrieveListOfRepos()
      },
      retrieveData () {
-     let baseURL = "https://api.github.com"
-     let username = this.inputtedUser
-     var urlUserData = baseURL + "/users/" + username
+     var urlUserData = this.baseURL + "/users/" + this.inputtedUser
      axios.get(urlUserData,{
        headers: {
          authorization: "token "+ process.env.VUE_APP_API_KEY
        },
-       timeout: 1000
+       timeout: 10000
      })
      .then(response => {
        this.userData = response
@@ -132,44 +138,75 @@ import axios from "axios"
        this.dateCreated = this.userData.data.created_at
      })
      },
-     retrieveRepoData () {
-     let baseURL = "https://api.github.com"
-     let username = this.inputtedUser
-     var urlRepoData = baseURL + "/users/" + username +"/repos"
-       axios.get(urlRepoData,{
-       headers: {
-         authorization: "token "+ process.env.VUE_APP_API_KEY
-       },
-       timeout: 1000
-     })
-     .then(response => {
-       this.repoData = response
-     })
-     },
-     retrieveCommitData (page) {
-     let baseURL = "https://api.github.com"
-     let username = this.inputtedUser
-     var urlCommitData = baseURL + "/users/" + username +"/events?page=" + page 
-     return axios.get(urlCommitData,{
-       headers: {
-         authorization: "token "+ process.env.VUE_APP_API_KEY
-       },
-       timeout: 1000
-     })
-     .then(response => {
-       if(response.data.length != 0){
-        this.commitData.push (response.data)
-        console.log(this.commitData)
-       }
-     })
-     },
-      retrieveAllData() {
-      this.commitData = []
-      for(var i = 1; i <= 10; i++){
-        this.retrieveCommitData(i)
-      }
+    retrieveListOfRepos (){
+      var urlCommitData = this.baseURL + "/users/" + this.inputtedUser +"/repos"
+      return axios.get(urlCommitData,{
+      headers: {
+        authorization: "token "+ process.env.VUE_APP_API_KEY
+      },
+      timeout: 10000
+      })
+      .then(response => {
+        this.listOfRepos = response.data
+        setTimeout(this.retrieveLanguages(), 2000)
+        setTimeout(this.retrieveCommitData(), 2000)
+      })
+    },
+    retrieveCommitData () {
+        var commits = new Map()
+        for (var i = 0; i < this.listOfRepos.length; i++){
+        let repoName = this.listOfRepos[i].name
+        axios.get(this.baseURL + "/repos/" + this.login + "/" + this.listOfRepos[i].name + "/stats/contributors" ,{
+        headers: {
+          authorization: "token "+ process.env.VUE_APP_API_KEY
+        },
+        timeout: 10000         
+        })
+        .then(response => {
+          let commitStats = response.data;
+          for (var j = 0; j < commitStats.length; j++){
+            if (commitStats[j] !== undefined && commitStats[j].author.login === this.login) {
+             commits.set(repoName, commitStats[j].total);
+            }
+          }
+          this.repoCommits = [];
+          for (const [key, value] of commits){
+            if (key !== "undefined" && key !== undefined) {
+              this.repoCommits.push([key, value])
+            }
+          }
+        })            
+        }
+      },
+      retrieveLanguages () {
+        var languages = new Map();
+        for (var i = 0; i < this.listOfRepos.length; i++){
+        axios.get(this.baseURL + "/repos/" + this.login + "/" + this.listOfRepos[i].name + "/languages", {
+        headers: {
+          authorization: "token "+ process.env.VUE_APP_API_KEY
+        },
+        timeout: 10000          
+        }).then(response => {
+          for(const [key, value] of Object.entries(response.data)){
+            if(key !== undefined && key !== "undefined") {
+              if(languages.has(key)){
+                let num = languages.get(key);
+                languages.set(key, num + value)
+                } else {
+                  languages.set(key, value)
+                }
+              }
+            }
+          this.languages  = [];
+          for(const [key, value] of languages){
+            if(key !== undefined && key !== "undefined") {
+              this.languages.push([key,value])
+            }
+          }
+      })
     }
    }
+  }
 }
 </script>
 <style scoped>
